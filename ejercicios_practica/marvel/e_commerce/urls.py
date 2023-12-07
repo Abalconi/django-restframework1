2edbe28b553e40759bb5d6aeac958fa7ffebe909
Alessandra Balconi <127406809+Abalconi@users.noreply.github.com>
2023-12-07 10:09:50 -0600

    Update urls.py

--- ejercicios_practica/marvel/e_commerce/urls.py
+++ ejercicios_practica/marvel/e_commerce/urls.py
@@ -1,14 +1,12 @@
-from django.urls import path, include
-from .views import hello_world_view, request_data_view, get_comics, purchased_item
+from django.urls import path
+from e_commerce.hello_world_views import *
+from e_commerce.marvel_views import *
 
 urlpatterns = [
     path('hello-world/', hello_world_view),
     path('request-data/', request_data_view),
     path('get-comics/', get_comics),
-    path('purchased-item/', purchased_item),
-    path('api/', include('e_commerce.api.urls')),  # Incluir las rutas de la API
-    # Otras rutas específicas de tu aplicación e_commerce...
-]
+    path('purchased-item/', purchased_item)
 
 
 
